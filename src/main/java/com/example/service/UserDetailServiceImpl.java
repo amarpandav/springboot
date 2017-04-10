a07d62115e9dbaaf9b5ec342f3248f45cc1f938b
Amar Pandav <amarpandav@gmail.com>
2017-04-10 15:56:22 +0200

    junits for mockmvc - use of spring security

--- src/main/java/com/example/service/UserDetailServiceImpl.java
+++ src/main/java/com/example/service/UserDetailServiceImpl.java
@@ -33,7 +33,7 @@ public class UserDetailServiceImpl implements UserDetailsService {
     @Transactional(readOnly = true)
     public UserDetails loadUserByUsername(String username)
             throws UsernameNotFoundException, DataAccessException {
-        logger.info("GETTING LOGIN DETAILS!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!");
+        logger.info("************************** GETTING LOGIN DETAILS for : "+username);
 
         List<GrantedAuthority> gas = new ArrayList<GrantedAuthority>();
         if ("amar".equals(username)) {
@@ -43,7 +43,7 @@ public class UserDetailServiceImpl implements UserDetailsService {
             gas.add(new SimpleGrantedAuthority("USER"));
         }
 
-        String password = "amar123"; //TODO in reality
+        String password = username+"123"; //TODO in reality
         UserDetails userDetails = new org.springframework.security.core.userdetails.User(
                 username, password, true, true, true, true, gas);
 

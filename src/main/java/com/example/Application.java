d25198f19658473f678d6ec17e067503c822772a
Amar Pandav <amarpandav@gmail.com>
2017-04-17 13:58:19 +0200

    migrate to hsqldb

--- src/main/java/com/example/Application.java
+++ src/main/java/com/example/Application.java
@@ -3,6 +3,11 @@ package com.example;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+import javax.annotation.PostConstruct;
+import org.hsqldb.util.DatabaseManagerSwing;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.context.ConfigurableApplicationContext;
+
 /**
  * This class is both a bootstrap class and a configuration class.
  *
@@ -16,8 +21,18 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class Application {
 
 	public static void main(String[] args) {
-
 	    //Bootstrap the application
-		SpringApplication.run(Application.class, args);
+		//SpringApplication.run(Application.class, args);
+
+        //When i used DatabaseManagerSwing I got java.awt.HeadlessException hence using SpringApplicationBuilder
+        SpringApplicationBuilder builder = new SpringApplicationBuilder(Application.class);
+        builder.headless(false);
+        ConfigurableApplicationContext context = builder.run(args);
 	}
+
+    @PostConstruct
+    public void getDbManager(){
+        DatabaseManagerSwing.main(
+                new String[] { "--url", "jdbc:hsqldb:mem:application", "--user", "sa", "--password", ""});
+    }
 }
---
+++ src/main/java/com/example/config/Profiles.java
@@ -0,0 +1,6 @@
+package com.example.config;
+
+public class Profiles {
+    public static final String PROFILE_HSQL = "development";
+    public static final String ORACLE_DVSZ_C = "production";
+}
